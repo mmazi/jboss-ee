537e272eb1a1b397a6ab5f49de78ccdc16630feb
matija.mazi@gmail.com <matija.mazi@gmail.com>
2012-11-28 19:43:27 +0100

    simplify packages
    Conflicts:
    
    	model/src/main/java/javaee6demo/model/Order.java
    	model/src/main/java/javaee6demo/model/OrderItem.java
    	model/src/main/java/javaee6demo/model/Product.java
    	model/src/main/java/javaee6demo/model/ProductCategory.java
    	model/src/main/java/javaee6demo/model/Unit.java
    	model/src/main/java/javaee6demo/model/User.java
    	web/src/main/webapp/WEB-INF/web.xml

--- model/src/main/java/com/housing/javaee6demo/model/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlAccessorType(XmlAccessType.FIELD)
-package com.housing.javaee6demo.model;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
---
+++ web/src/main/java/javaee6demo/ejb/cdi/Authenticator.java
@@ -0,0 +1,73 @@
+package javaee6demo.ejb.cdi;
+
+import javaee6demo.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Produces;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+
+/**
+ * @author Matija Mazi <br/>
+ * @created 9.2.12 12:45
+ */
+@SessionScoped
+@Stateful
+@Named
+public class Authenticator implements Serializable {
+    private static final Logger log = LoggerFactory.getLogger(Authenticator.class);
+
+    @Inject private Instance<Credentials> credentialsInstance;
+
+    @PersistenceContext private EntityManager em;
+
+    @Inject private Event<LoginEvent> loginEvent;
+
+
+    private User user;
+
+    private String username;
+
+    public String login() throws ServletException {
+        final Credentials cred = credentialsInstance.get();
+        user = em.createQuery("select u from User u where u.username = :un and u.password = :pw", User.class)
+                .setParameter("un", cred.getUsername())
+                .setParameter("pw", cred.getPassword())
+                .getSingleResult();
+        username = user.getUsername();
+        getRequest().login(cred.getUsername(), cred.getPassword());
+        loginEvent.fire(new LoginEvent(user));
+        log.debug("User {} logged in.", user.getName());
+        return "logged-in";
+    }
+
+    public String logout() {
+        getRequest().getSession().invalidate();
+        return "/login.jsf";
+    }
+
+    private HttpServletRequest getRequest() {
+        return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+    }
+
+    @Produces @Authenticated @Named
+    public User getAuthenticatedUser() {
+        return user;
+    }
+
+    @Produces @Authenticated @Named
+    public String getUsername() {
+        return username;
+    }
+}
