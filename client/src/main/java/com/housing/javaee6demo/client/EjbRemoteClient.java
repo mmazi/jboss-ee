d8df2d10346b0bd145bf772979a3b1a59746b1b0
matija.mazi@gmail.com <matija.mazi@gmail.com>
2012-11-28 19:43:27 +0100

    simplify packages

--- client/src/main/java/com/housing/javaee6demo/client/EjbRemoteClient.java
+++ client/src/main/java/com/housing/javaee6demo/client/EjbRemoteClient.java
@@ -1,7 +1,7 @@
-package com.housing.javaee6demo.client;
+package javaee6demo.client;
 
-import com.housing.javaee6demo.ejb.remote.Hello;
-import com.housing.javaee6demo.ejb.remote.HelloGoodbye;
+import javaee6demo.ejb.remote.Hello;
+import javaee6demo.ejb.remote.HelloGoodbye;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -23,13 +23,13 @@ public class EjbRemoteClient {
     }
 
     private static void invokeStateless(String who) throws NamingException {
-        Hello hello = InitialContext.doLookup("ejb:/javaee6-demo//HelloBean!com.housing.javaee6demo.ejb.remote.Hello");
+        Hello hello = InitialContext.doLookup("ejb:/javaee6-demo//HelloBean!javaee6demo.ejb.remote.Hello");
         System.out.println("Got " + hello);
         System.out.println(hello.sayHello(who));
     }
 
     private static HelloGoodbye invokeStateful(String who) throws NamingException {
-        HelloGoodbye helloGoodbye = InitialContext.doLookup("ejb:/javaee6-demo//HelloGoodbyeBean!com.housing.javaee6demo.ejb.remote.HelloGoodbye?stateful");
+        HelloGoodbye helloGoodbye = InitialContext.doLookup("ejb:/javaee6-demo//HelloGoodbyeBean!javaee6demo.ejb.remote.HelloGoodbye?stateful");
         System.out.println("Got " + helloGoodbye);
         System.out.println(helloGoodbye.getWho());
         System.out.println(helloGoodbye.sayHello(who));
---
+++ model/src/main/java/javaee6demo/model/Order.java
@@ -0,0 +1,137 @@
+package javaee6demo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Matija Mazi <br/>
+ * @created 9.1.12 17:22
+ */
+@Entity
+@Table(name = "ORDERS")
+public class Order implements Serializable {
+    @Id @Size(max = 40)
+    private String id;
+
+    private Date created;
+
+    @Version
+    private Timestamp updated;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("product")
+    private List<OrderItem> items = new ArrayList<OrderItem>();
+
+    @ManyToOne
+    private User creator;
+
+    @ManyToOne
+    private User assignee;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 31)
+    private Status status;
+
+    private String comments;
+
+    protected Order() { }
+
+    public Order(User creator) {
+        this.creator = creator;
+        this.assignee = creator;
+        created = new Date();
+        status = Status.RCV;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public boolean addItem(OrderItem item) {
+        item.setOrder(this);
+        return items.add(item);
+    }
+
+    public void addItem(Product product, double quantity) {
+        addItem(new OrderItem(this, product, quantity));
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Order[{0} {1}, created {2}, assigned to {3}]", id, status, created, assignee);
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.id = MessageFormat.format("O/{0,date,medium}_{0,time,medium}", new Date());
+    }
+
+    public enum Status {
+        RCV("Received"),
+        PAID("Paid"),
+        PKG("Packaging"),
+        SNT("Sent"),
+        DLV("Delivered"),
+        ;
+
+        private String description;
+
+        Status(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+}
