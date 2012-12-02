e2ac80c369db3d66ae7609c4da0bd52ee71715d5
matija.mazi@gmail.com <matija.mazi@gmail.com>
2012-12-02 11:03:08 +0100

    simplify package

---
+++ web/src/main/java/javaee6demo/ejb/remote/HelloGoodbye.java
@@ -0,0 +1,11 @@
+package javaee6demo.ejb.remote;
+
+/**
+ * @author Matija Mazi <br/>
+ * @created 9.2.12 7:30
+ */
+public interface HelloGoodbye extends Hello {
+    String getWho();
+
+    String sayGoodbye();
+}
